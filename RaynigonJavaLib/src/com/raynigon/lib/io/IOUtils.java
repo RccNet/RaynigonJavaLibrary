cee037c69a857f7ca06bed6c361430273939a14c
Simon Schneider <sim@raynigon.de>
2016-04-01 20:36:42 +0200

    Added the JsonFieldAttribute

--- RaynigonJavaLib/src/com/raynigon/lib/io/IOUtils.java
+++ RaynigonJavaLib/src/com/raynigon/lib/io/IOUtils.java
@@ -1,13 +1,18 @@
 package com.raynigon.lib.io;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 
-/**Generated on 04.11.2015 by Simon Schneider in Project <b>RaynigonJavaLib</b><p>
+/**The IOUtils is a Collections of Functions to work with Input and Output Streams
+ * Generated on 04.11.2015 by Simon Schneider in Project <b>RaynigonJavaLib</b><p>
  * @author Simon Schneider
  */
 public class IOUtils {
@@ -15,6 +20,12 @@ public class IOUtils {
 	/** The Default Buffer Size for Copy Operations on Streams
 	 */
 	public static final int DEFAULT_BUFFER_SIZE = 4096;
+	
+	
+	public static void copy(String input, OutputStream os, Charset inCs) throws IOException{
+		ByteArrayInputStream bis = new ByteArrayInputStream(input.getBytes(inCs));
+		copy(bis, os);
+	}
 	
 	public static void copy(InputStream inIs, StringWriter inSw, Charset inCs) throws IOException {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -38,5 +49,17 @@ public class IOUtils {
 			os.write(bytes, 0, length);
 		
 		return;
+	}
+	
+	public void writeToFile(InputStream is, File file) throws IOException{
+		FileOutputStream fos = new FileOutputStream(file);
+		copy(is, fos);
+		fos.close();
+	}
+	
+	public void readFromFile(File file, OutputStream os) throws IOException{
+		FileInputStream fis = new FileInputStream(file);
+		copy(fis, os);
+		fis.close();
 	}
 }
