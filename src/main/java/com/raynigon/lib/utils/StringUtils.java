40aa4e9a09c567bfd041aa2c5724a80e592dd9d3
Simon <sim@raynigon.de>
2017-08-30 20:42:19 +0200

    Added own StringUtils

---
+++ src/main/java/com/raynigon/lib/utils/StringUtils.java
@@ -0,0 +1,9 @@
+package com.raynigon.lib.utils;
+
+
+public class StringUtils {
+    
+    public static boolean isNullOrEmpty(String toTest) {
+        return (toTest == null || toTest.length() == 0);
+    }
+}
